683c395fa570b165c408ce674648c831d84ab25d
Christoph Singewald <christoph@singewald.at>
2020-05-12 22:47:56 +0200

    input error handling

--- src/renderer/components/layerlist/LayerNameEditor.js
+++ src/renderer/components/layerlist/LayerNameEditor.js
@@ -13,11 +13,22 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
+
+const validators = [
+  { func: (value) => value.length > 10, error: 'The value must be shorter than 10 characters.' },
+  { func: (value) => value.length < 2, error: 'The value must be longer than 2 characters.' }
+]
+
+function validate (value) {
+  return validators.map(v => v.func(value) ? v.error : null).filter(x => x != null)
+}
+
 /**
  *
  */
 export const LayerNameEditor = props => {
   const classes = useStyles()
+
 
   const handleChange = event => props.update(event.target.value)
   const handleKeyDown = event => {
@@ -27,15 +38,24 @@ export const LayerNameEditor = props => {
     }
   }
 
+  const errors = validate(props.value)
+  const hasError = () => errors != null && errors.length > 0
+
+  const style = (hasError()) ? { borderBottom: '1px solid red' } : {}
+  console.log(props.value, props.value.length)
   return (
-    <InputBase
-      className={classes.editor}
-      value={props.value}
-      autoFocus
-      onKeyDown={handleKeyDown}
-      onChange={handleChange}
-      onBlur={props.cancel}
-    />
+    <>
+      <InputBase
+        className={classes.editor}
+        style={ style }
+        value={props.value}
+        autoFocus
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        onBlur={props.cancel}
+      />
+      <div style={ { display: hasError() ? 'block' : 'none', width: '100%', textAlign: 'start', color: 'red', fontSize: '0.8em', paddingLeft: '8px', paddingRight: '8px' } }>{ errors[0] }</div>
+    </>
   )
 }
 
